e3274ef8b739018f32510ccf88eef348ce6ae48a
omid1212 <omidwezd@mihanmail.ir>
2015-12-03 16:50:12 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {118682430},
+    sudo_users = {70480064},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
